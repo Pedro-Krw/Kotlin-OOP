42190d0f7e1404cf02d4ae6b5a9fe0cbc0c2f837
pedrokorwa <korwapedro@gmail.com>
2021-06-01 20:47:45 +0700

    new

--- Kotlin_/src/Kotlin One.kt
+++ Kotlin_/src/Kotlin One.kt
@@ -1,2 +1,16 @@
-class `Kotlin One` {
+const val nama_ = "saitama"
+const val umur_ = 19
+
+
+fun main() {
+
+    val nama = "sunda"
+    val umur = 19
+
+    if (umur_ < umur){
+        print("ini adalah " + nama_)
+    }else  if (umur_ > umur){
+        print("ini adalah " + nama)
+    }
+
 }

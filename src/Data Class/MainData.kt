6bab9d1be30cf0bca36d44618bdc32e7bb4c8037
pedrokorwa <korwapedro@gmail.com>
2021-06-03 21:23:43 +0700

    new

--- src/Data Class/MainData.kt
+++ src/Data Class/MainData.kt
@@ -1,3 +1,19 @@
 fun main() {
     print("Data")
+
+    nameList()
+}
+
+
+fun nameList(): ArrayList<String>{
+   val names = ArrayList<String>()
+    names.add("martin")
+    names.add("jaen")
+    names.add("zoro")
+    names.add("kiba")
+    names.add("war")
+
+    print(names)
+
+  return names
 }

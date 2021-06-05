7f3348eb1ab2bd44a48f08926f7b021239c15b19
pedrokorwa <korwapedro@gmail.com>
2021-06-05 20:32:21 +0700

    new

--- Root/src/DataAccess.kt
+++ Root/src/DataAccess.kt
@@ -1,2 +1,25 @@
+fun main() {
+    DataAccess().root()
+}
+
+
+
 class DataAccess {
+
+    fun root(){
+
+        val botton = true
+        val No = 0 until 100
+        val compare = 5 until 100
+
+        if (No != compare){
+            print(45 *2)
+        }
+
+
+
+    }
+
+
+
 }

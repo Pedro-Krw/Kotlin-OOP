c941331ebc020d1733a0818203a6f124a8d7b8bf
pedrokorwa <korwapedro@gmail.com>
2021-06-05 22:19:11 +0700

    new

--- Root/src/App/Access.kt
+++ Root/src/App/Access.kt
@@ -1,16 +1,23 @@
 package App
 
+import RootAccess.Data
 import RootAccess.app
 
 fun main() {
 
-    val Run = app()
-    Run.Display = 70
-    Run.Setup = "  SuperAmoled  "
-    Run.Vendor = "  Panasonic"
+//    val Run = app()
+//    Run.Display = 70
+//    Run.Setup = "  SuperAmoled  "
+//    Run.Vendor = "  Panasonic"
+//
+//    print("${Run.Display} Innci")
+//    print(Run.Setup)
+//    print(Run.Vendor)
 
-    print("${Run.Display} Innci")
-    print(Run.Setup)
-    print(Run.Vendor)
+
+
 
 }
+
+
+
--- Root/src/RootAccess/Data.kt
+++ Root/src/RootAccess/Data.kt
@@ -1,9 +1,33 @@
 package RootAccess
 
+fun main() {
+    Data()
+
+}
+
 class Data {
 
-    var nama : String = "Kiba"
-    var umur : Int = 20
-    var alamat : String = "Biak"
+
+    fun Zoro(){
+        var umur3 : Int = 20
+    }
+
+
+    fun Kiba(){
+        var nama1 : String = "Kiba"
+        var umur1 : Int = 17
+        var NilaiUjian1  : String = "80"
+
+    }
+
+
+    fun War(){
+        var nama2 : String = "War"
+        var umur2 : Int = 25
+        var NilaiUjian2  : String = "65"
+
+    }
+
+
 
 }

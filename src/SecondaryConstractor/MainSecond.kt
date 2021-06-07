ff730673f424f7c185b4933a2213727470376eb5
pedrokorwa <korwapedro@gmail.com>
2021-06-07 21:57:11 +0700

    new

---
+++ src/SecondaryConstractor/MainSecond.kt
@@ -0,0 +1,19 @@
+package SecondaryConstractor
+
+class MainSecond (){
+
+
+    constructor( nama: String ,Alamat: String ,  Jalan: String){
+
+        var  nama = nama
+        var Alamat = Alamat
+        var Jalan = Jalan
+
+    }
+
+
+
+
+
+
+}
---
+++ src/SecondaryConstractor/Second.kt
@@ -0,0 +1,12 @@
+package SecondaryConstractor
+
+fun main() {
+  DataOrang()
+}
+
+  fun DataOrang(){
+      val Orang = MainSecond("barnabas" , "BIAK KOTA" , "MANJAPAHIT")
+
+      println(Orang)
+
+  }

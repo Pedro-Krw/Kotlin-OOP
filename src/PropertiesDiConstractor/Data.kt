8c91025afc5aa5c7b6883b27d06f6431074bfa63
pedrokorwa <korwapedro@gmail.com>
2021-06-07 23:45:19 +0700

    new

--- src/PropertiesDiConstractor/Data.kt
+++ src/PropertiesDiConstractor/Data.kt
@@ -1,4 +1,7 @@
 package PropertiesDiConstractor
 
-class Data {
+class Data (var user :String = "Ini nama default => anonim  " , var id : String = "ini Id default => 123456 \n"){
+
+
+
 }
--- src/PropertiesDiConstractor/Main.kt
+++ src/PropertiesDiConstractor/Main.kt
@@ -1,2 +1,20 @@
 package PropertiesDiConstractor
 
+fun main() {
+
+    DataUser()
+
+}
+
+   fun DataUser(){
+
+       val user1 = Data("Edan " , "02281\n")
+       val user2 = Data("Anonim terdaftar ")
+       val user3 = Data()
+
+       print(user1.user + user1.id)
+       print(user2.user + user2.id)
+       print(user3.user + user3.id)
+
+
+   }

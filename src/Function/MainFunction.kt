0497706434da317dd2912ef61664ce4705c016c2
pedrokorwa <korwapedro@gmail.com>
2021-06-08 20:49:49 +0700

    new

---
+++ src/Function/MainFunction.kt
@@ -0,0 +1,16 @@
+package Function
+
+ fun main() {
+      Call()
+  }
+
+  fun Call(){
+
+       val data = Sa("" ,  "ini di ubah")
+
+
+      print(data.Orang)
+      print(data.Usia)
+
+
+  }
--- src/Function/Sa.kt
+++ src/Function/Sa.kt
@@ -1,4 +1,23 @@
 package Function
 
 class Sa {
+
+    var Orang : String = " "
+    var Usia : String = " "
+
+    constructor(name : String = " dflt1 " , umur : String = "dflt2"){
+        Orang = name
+        Usia = umur
+    }
+
+
+    fun data1(){
+        var nama = "bob"
+        var umur = 23
+        var jenis = "laki"
+
+        print("$nama , $umur  , $jenis")
+    }
+
+
 }

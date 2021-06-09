a4aa0e66dd38e702790e4bed60821ea298992710
pedrokorwa <korwapedro@gmail.com>
2021-06-09 18:31:21 +0700

    new

---
+++ src/Inheritance/Inheritance.kt
@@ -0,0 +1,4 @@
+package Inheritance
+
+class Inheritance {
+}
--- src/ThisKeyword/Kyword.kt
+++ src/ThisKeyword/Kyword.kt
@@ -3,4 +3,13 @@ package ThisKeyword
 // This kyword adalah kyword yang mendefenisikan object saat ini
 
 class Kyword {
+
+    var nama : String = ""
+
+    open fun getdata ()
+    {
+
+    }
+
+
 }

f2070928f35d789213c1b5411c3f5c8b53d183b9
pedrokorwa <korwapedro@gmail.com>
2021-06-09 22:33:19 +0700

    new

--- src/FunctionOverriding/MainOver.kt
+++ src/FunctionOverriding/MainOver.kt
@@ -1,2 +1,20 @@
 package FunctionOverriding
 
+
+fun main() {
+
+    getDataFromOver()
+}
+
+
+ fun getDataFromOver(){
+
+     val Over = Over( )
+
+     print(Over.firtsnama)
+     print(Over.age)
+
+
+
+
+ }
--- src/FunctionOverriding/Over.kt
+++ src/FunctionOverriding/Over.kt
@@ -1,4 +1,40 @@
 package FunctionOverriding
 
-class Over {
+open  class Over {
+
+    var firtsnama : String = ""
+    var age : String = ""
+
+    constructor( lastName : String = "nameUser" , middleName : String = "UserAge"){
+
+        firtsnama = lastName
+        age = middleName
+
+
+    }
+
+
+    fun  getOver (){
+
+        print(" Sa Pnya umur : $age")
+        print("sa pu nama $firtsnama")
+
+    }
+
+
+
+    class Overriding () : Over ("" , ""){
+
+        override fun toString(): String {
+
+            print("$firtsnama = ini yang di ubah")
+
+            return super.toString()
+        }
+
+
+    }
+
+
+
 }

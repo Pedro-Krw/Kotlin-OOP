4a675f87bd7d06519384fae217b29c2476141757
pedrokorwa <korwapedro@gmail.com>
2021-06-11 22:35:24 +0700

    new

--- src/AnyClass/Any.kt
+++ src/AnyClass/Any.kt
@@ -3,11 +3,13 @@ package AnyClass
  open  class mainSaja (val name :String ) : Any(){
 
     fun get(){
+
         val merek_1 = "samsung"
         val merek_2 = "huwawei"
         val merek_3 = "nokia"
         val merek_4 = "sony"
         val merek_5 = "iphone"
+        print("$merek_1 , $merek_2 , $merek_3 , , $merek_4 , $merek_5")
 
 
 
@@ -18,10 +20,9 @@ package AnyClass
 
 class any : mainSaja(""){
 
-    fun dataSeler(){
+    override fun toString(): String {
 
-        print(get())
-
+        return "program ini adalah ${get().toString()}"
     }
 
 }

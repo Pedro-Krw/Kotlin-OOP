7ca25bf15b935cffa7dd0ff80a1efae64fb5ce9e
pedrokorwa <korwapedro@gmail.com>
2021-06-16 21:17:47 +0700

    new

--- src/equals/Equals.kt
+++ src/equals/Equals.kt
@@ -1,2 +1,24 @@
 package equals
 
+fun main() {
+
+    dataUser()
+}
+
+
+  private fun dataUser(){
+
+      val data = "get Access "
+      val key : Boolean = true
+      val pass = 1234567
+
+      val tampung = data + key
+
+      if (data == key.toString() ){
+          print(tampung)
+      }
+      print(tampung)
+
+
+
+  }

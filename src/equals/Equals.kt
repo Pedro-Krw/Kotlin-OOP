4ae4871f12622a2f53f584f359c2c669f6935104
pedrokorwa <korwapedro@gmail.com>
2021-06-16 21:30:37 +0700

    new

--- src/equals/Equals.kt
+++ src/equals/Equals.kt
@@ -1,24 +1,31 @@
 package equals
 
 fun main() {
-
     dataUser()
+
 }
 
 
-  private fun dataUser(){
 
-      val data = "get Access "
-      val key : Boolean = true
-      val pass = 1234567
+private fun dataUser(){
 
-      val tampung = data + key
-
-      if (data == key.toString() ){
-          print(tampung)
-      }
-      print(tampung)
+//    Ini bagian dari object yan akan menmpung nilai dari class lain
+    var tampug = mainEquals()
+    print(tampug.Tulisan())
 
 
 
-  }
+    val data = "get Access"
+    val key : Boolean = true
+    val pass = 1234567
+
+    val tampung = data + key
+
+    if (data == key.toString() ){
+        print(pass.toString() + data)
+    }
+    print(pass.toString() + data)
+
+
+
+}
--- src/equals/mainEquals.kt
+++ src/equals/mainEquals.kt
@@ -1,4 +1,12 @@
 package equals
 
 class mainEquals {
+
+
+    val string = "ini adalah bagian dari class lain yang di panggil \n "
+
+    fun Tulisan(){
+        print(string)
+    }
+
 }

a0a7993e029e9e8d4a25ff0948c3836fa6e43b57
pedrokorwa <korwapedro@gmail.com>
2021-06-17 21:00:22 +0700

    new

--- src/hashCode/HashCode.kt
+++ src/hashCode/HashCode.kt
@@ -1,7 +1,12 @@
 package hashCode
 
 // hashcode adalah function yang di gunakan sebagai representasi dari angka unit dari object yang kita buat
-class HashCode {
+
+class HashCode (val namaku : String) {
+
+    override fun hashCode(): Int {
+        return namaku.hashCode()
+    }
 
 
 
--- src/hashCode/MainHashCode.kt
+++ src/hashCode/MainHashCode.kt
@@ -1,12 +1,16 @@
 package hashCode
 
 fun main() {
-
+   getDataFromClass()
 }
 
  private  fun getDataFromClass(){
 
+  val getNama = HashCode("pedro \n")
+  val getNama2 = HashCode("pedro \n")
 
-
+   print(getNama.hashCode() == getNama2.hashCode())
+  print(getNama.hashCode())
+  print(getNama2.hashCode())
 
  }

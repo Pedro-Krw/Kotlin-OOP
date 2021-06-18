3272dbf9386f7f7461e4b7a99a7ef581bcd07a85
pedrokorwa <korwapedro@gmail.com>
2021-06-18 20:27:07 +0700

    new

--- src/AbstractPropertiesDanFunction/MainApDf.kt
+++ src/AbstractPropertiesDanFunction/MainApDf.kt
@@ -3,5 +3,16 @@ package AbstractPropertiesDanFunction
 
 //    Bagian ini yang akan pertamakali di jalnkan ketika aplikasi di run
 fun main() {
+  runData()
 
 }
+
+ private fun runData(){
+
+      val cat = Cat()
+      val dog = Dog()
+
+     cat.run()
+     dog.run()
+
+ }

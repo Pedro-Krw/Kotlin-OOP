d75021e9e36f86932a2d320f1aec01187ee0cf57
pedrokorwa <korwapedro@gmail.com>
2021-06-21 23:12:00 +0700

    new

--- src/ExtensionFunction/Extension_Function.kt
+++ src/ExtensionFunction/Extension_Function.kt
@@ -1,5 +1,10 @@
 package ExtensionFunction
 
-class Extension_Function {
+class Student(val nama : String , val age :Int) {
+
+    fun Student.sayHEllo(name : String = "kiba"){
+
+         print("hello $name my name is $nama and my age is $age")
+    }
 
 }
--- src/ExtensionFunction/MainEf.kt
+++ src/ExtensionFunction/MainEf.kt
@@ -1,5 +1,16 @@
 package ExtensionFunction
 
-class MainEf {
-
+fun main() {
+    dataGet()
 }
+
+ private fun dataGet(){
+     val newobject = Student("joko" , 20)
+     print(newobject.age)
+     print(newobject.nama)
+
+
+
+
+
+ }

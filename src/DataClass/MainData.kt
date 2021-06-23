3cde50865b8f46a54e86e0145c0f7d55e3f51a96
pedrokorwa <korwapedro@gmail.com>
2021-06-23 21:44:33 +0700

    new

--- src/DataClass/MainData.kt
+++ src/DataClass/MainData.kt
@@ -11,7 +11,6 @@ private fun callDataClass(){
      val CallObject = Data_Class( "Masako" , " 23 Juni 2021" , " 1000" )
      val CallObject2 = CallObject.copy(nama_Barang = "Royko")
 
-
    println(CallObject)
     print(CallObject2)
 

affb56af7a5bad2f79630dc60209eee49431dde2
pedrokorwa <korwapedro@gmail.com>
2021-09-30 21:26:34 +0700

    new

---
+++ src/Laptop.kt
@@ -0,0 +1,24 @@
+
+
+fun main() {
+
+}
+
+class Laptop (
+
+    val text : String = "on",
+    val text_2 : String = "oof",
+    val layar : String,
+    val Google_chrome : String,
+    val led : String,
+    val vsCode : String,
+    val usb : Int)
+{
+   fun on (){
+       print(text)
+   }
+
+   fun off(){
+       print("$text_2")
+   }
+}
